8cad74f27ea9a043d3cab90b691cd55e32f57539
NizarXD <115076357+NizarXD@users.noreply.github.com>
2024-10-15 07:28:38 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="w-full py-10 text-white flex items-center justify-center bg-[#121928] mt-10">
-        <span className="mr-2 text-lg font-bold">E-AlQur&apos;an</span>
+        <span className="mr-2 text-lg font-bold">AlQur&apos;an</span>
         <span className="text-xs">Copyright 2024 ZarxdOfficial</span>
       </div>
     </>
